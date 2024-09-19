4599ca8738281da5f4533bb09093a777c883d4f3
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-09-19 09:33:18 +0500

    annotations for arrays

--- van-typescript/src/practical.ts
+++ van-typescript/src/practical.ts
@@ -1,8 +1,18 @@
 console.log('TS practical');
 
-let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
-orderStatus = 'shipped';
-orderStatus = 'delivered';
+// Variables
+// let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+// orderStatus = 'shipped';
+// orderStatus = 'delivered';
 
-let discount: number | string = 20;
-discount = '20%';
+// let name: string | undefined;
+// console.log(name?.length)
+
+// Arrays
+// let discount: number | string = 20;
+// discount = '20%';
+// let arr: number[] = [1, 2, 3, 4, 5];
+// arr.push()
+// let arr: (number | boolean)[] = [1, 2, 3, 4, 5, false, true, false];
+// console.log(arr);
+
