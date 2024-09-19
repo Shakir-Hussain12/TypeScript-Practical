02ce37bd7edc6eee23d5ace08910d7c5035c0c53
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-09-19 09:02:49 +0500

    annotations & types

---
+++ van-typescript/src/practical.ts
@@ -0,0 +1,8 @@
+console.log('TS practical');
+
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+
+let discount: number | string = 20;
+discount = '20%';
