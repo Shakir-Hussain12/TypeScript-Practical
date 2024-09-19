dea96aef096cf72a4b66cc29582a52fc50e89e69
Shakir Hussain <shakir.hussain6969@gmail.com>
2024-09-19 09:33:31 +0500

    Annotations for objects

--- van-typescript/src/practical.ts
+++ van-typescript/src/practical.ts
@@ -16,3 +16,16 @@ console.log('TS practical');
 // let arr: (number | boolean)[] = [1, 2, 3, 4, 5, false, true, false];
 // console.log(arr);
 
+//Objects
+// let car: {brand: string, model: string, year: number} = {brand: 'BMW', model: 'X5', year: 2020};
+// car.brand = 'Mazda';
+// car.brand = 123;
+// car.color = 'red';
+// console.log(car);
+
+// let item1 = {brand: 'Mazda', model: 'CX-5', year: 2020};
+// let item2 = {brand: 'Mitsubishi', model: 'MB-10'};
+
+// let items:{brand: string, model: string, year?: number}[] | undefined = [item1, item2];
+// items.push({brand: 'Toyota', model: 'Mitsubishi'})
+// console.log(items);
