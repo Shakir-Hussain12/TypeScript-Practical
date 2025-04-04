221d9cb7beb753a82b365f9b61c86b259fb8d950
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-04-04 17:47:27 +0500

    basic code for a tuple

---
+++ van-typescript/src/enum_tup.ts
@@ -0,0 +1,6 @@
+// basic codes for working with enum and tuples in typescript
+
+// tuple examples
+let tuple: [number, string] = [1, "Hi"];
+console.log(tuple[0]); // 1
+console.log(tuple[1]); // Hi 
