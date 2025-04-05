b900edd50a6f7c67507c436c94a5f812d4fa8473
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-04-05 22:37:39 +0500

    enum - first example

--- van-typescript/src/enum_tup.ts
+++ van-typescript/src/enum_tup.ts
@@ -2,13 +2,21 @@
 
 // tuple examples
 let tuple: [number, string] = [1, "Hi"];
-console.log(tuple[0]); // 1
-console.log(tuple[1]); // Hi
+// console.log(tuple[0]); // 1
+// console.log(tuple[1]); // Hi
 
 tuple.push('Yo');
 tuple.push(2);
 // tuple.push(true);  // can't add boolean
-console.log(tuple);
+// console.log(tuple);
 
-let person: [string, string, boolean] = ["John", "Married", true];
-console.log(person);
+// let person: [string, string, boolean] = ["John", "Married", true];
+// console.log(person);
+
+//enum examples
+enum Sample {
+    first,
+    second
+};
+
+console.log(Sample)
