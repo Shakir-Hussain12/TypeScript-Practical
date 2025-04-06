cadf1bdeefd0525eb6957a1980a1b23c1950211b
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-04-06 12:05:27 +0500

    asserted type as animal

--- van-typescript/src/type_assertions.ts
+++ van-typescript/src/type_assertions.ts
@@ -3,7 +3,12 @@ let ex: any = 'Hi, This is a string';
 // Using type assertion to treat 'ex' as a string
 let len: number = (ex as string).length
 console.log(len);
- 
+
+interface Animal {
+    name: string; 
+    breed: string;
+};
+
 let animalObject = `{
     "name": "Monty",
     "breed": "Labrador" 
@@ -14,8 +19,9 @@ let humanObject = `{
     "age": 30
 }`;
 
-let human = JSON.parse(humanObject);
-let animal = JSON.parse(animalObject);
+// Using type assertion to treat Objects as an Animal object
+let human = JSON.parse(humanObject) as Animal;
+let animal = JSON.parse(animalObject) as Animal;
 
-console.log(human);
+console.log(human); // No error, but you can't access age property because it is not part of the Animal interface
 console.log(animal);
