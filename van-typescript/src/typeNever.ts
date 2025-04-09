619f6c2c305316303029a86395a95f2bc4fabf63
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-04-09 22:15:58 +0500

    Example 2: typeNever

--- van-typescript/src/typeNever.ts
+++ van-typescript/src/typeNever.ts
@@ -12,3 +12,30 @@ const getData = (gen: Gender) => {
 
 console.log(getData('Male'));
 console.log(getData('Female'));
+
+// example 2
+enum GenderEnum {
+    Male,
+    Female,
+    Gay
+};
+
+const getDataEnum = (gen: GenderEnum) => {
+    switch (gen) {
+        case GenderEnum.Male:
+            return "It's a boy";
+        case GenderEnum.Female:
+            return "It's a girl";
+        case GenderEnum.Gay:
+            return "It's infected";
+        default:
+            // only works if all conditional values are covered
+            let checkVal: never = gen;
+            console.log(checkVal);
+            throw "It's a nothing";
+    }
+}
+
+console.log(getDataEnum(GenderEnum.Male));
+console.log(getDataEnum(GenderEnum.Female));
+console.log(getDataEnum(GenderEnum.Gay));
