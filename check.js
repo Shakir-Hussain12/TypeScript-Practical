933dbef086c63b899249405b91b69a16dfff7e7a
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-05-10 11:12:32 +0500

    refactored

--- check.js
+++ check.js
@@ -1,12 +1,12 @@
 let alphabets = {};
-    let s = "!m-rB`-oN!.W`cLAcVbN/CqSoolII!SImji.!w/`Xu`uZa1TWPRq`uRBtok`xPT`lL-zPTc.BSRIhu..-!.!tcl!-U62";
-    let k = 62;
-    let res = '';
+let s = "!m-rB`-oN!.W`cLAcVbN/CqSoolII!SImji.!w/`Xu`uZa1TWPRq`uRBtok`xPT`lL-zPTc.BSRIhu..-!.!tcl!-U62";
+let k = 62;
+let res = '';
 
-    
-    k = k % 26;
-    
-    for (let i = 97; i <= 122; i++) {
+
+k = k % 26;
+
+for (let i = 97; i <= 122; i++) {
     let temp = i;
 
     if (temp + k > 122) {
@@ -15,23 +15,23 @@ let alphabets = {};
 
     temp += k;
     alphabets[String.fromCharCode(i)] = String.fromCharCode(temp);
-    }
+}
 
-    for (let i = 0; i < s.length; i++) {
-        let temp = s.charCodeAt(i);
-        if (temp >= 65 && temp <= 90) {
-            temp = temp + 32;
-            temp = String.fromCharCode(temp);
-            
-            if (alphabets[temp]) {
-                res += alphabets[temp].toUpperCase();
-            }
-        } else if (alphabets[s[i]]) {
-            res += alphabets[s[i]];
-        } else {
-            res += s[i];
+for (let i = 0; i < s.length; i++) {
+    let temp = s.charCodeAt(i);
+    if (temp >= 65 && temp <= 90) {
+        temp = temp + 32;
+        temp = String.fromCharCode(temp);
+        
+        if (alphabets[temp]) {
+            res += alphabets[temp].toUpperCase();
         }
+    } else if (alphabets[s[i]]) {
+        res += alphabets[s[i]];
+    } else {
+        res += s[i];
     }
+}
 
-    console.log(s);
-    console.log(res);
+console.log(s);
+console.log(res);
