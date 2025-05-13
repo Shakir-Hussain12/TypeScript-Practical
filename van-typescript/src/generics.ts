825943ee365462ac2c6edb23b07af05965df95a6
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-05-13 20:33:18 +0500

    generic function examples

--- van-typescript/src/generics.ts
+++ van-typescript/src/generics.ts
@@ -2,3 +2,18 @@
 let strArr: Array<string> = ['a', 'b', 'c'];
 let numArr: Array<number> = [1, 2, 3];
 let boolArr: Array<boolean> = [true, false, true];
+
+// example functions
+// method 1
+function getStrFunc<T>(params: T): T {
+    return params;
+}
+
+//method 2
+const getNumFunc= <T>(args: T): T => args;
+
+
+let ans1 = getStrFunc<string>('Hello');
+console.log(ans1)
+let ans2 = getNumFunc<number>(123);
+console.log(ans2);
