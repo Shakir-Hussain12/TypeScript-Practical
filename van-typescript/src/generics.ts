b86d1a8a3844b3d411e01c32519da42054e5e741
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-05-13 20:41:07 +0500

    custom interface example

--- van-typescript/src/generics.ts
+++ van-typescript/src/generics.ts
@@ -17,3 +17,23 @@ let ans1 = getStrFunc<string>('Hello');
 console.log(ans1)
 let ans2 = getNumFunc<number>(123);
 console.log(ans2);
+
+//custom generics
+interface customInterface<T> {
+    name: T,
+    age: number,
+    getDetails: () => {name: T, age: number}
+}
+
+let myObj: customInterface<string> = {
+    name: 'Shakir',
+    age: 26,
+    getDetails() {
+        return {
+            name: this.name,
+            age: this.age
+        }
+    }
+}
+
+console.log(myObj.getDetails());
