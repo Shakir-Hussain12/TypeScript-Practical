ac6b1021353e2f6dbd1b9f4403d42ccb324f5a0e
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-05-14 21:00:33 +0500

    second method

--- van-typescript/src/genericTypes.ts
+++ van-typescript/src/genericTypes.ts
@@ -36,3 +36,12 @@ function getName<T extends Student | Product | Car>(obj: T): string {
 console.log(getName(student)); //will work as T extends student
 console.log(getName(product)); //won't work even though product has name property, unless it is extended to T
 console.log(getName(car)); // no name property in car
+
+// second method - using object or named property
+let getNameProp = <T extends {name: string}>(obj: T): string => {
+    return obj.name;
+}
+console.log(getNameProp(student));
+console.log(getNameProp(product));
+// car has no name property, so it will throw an error
+// console.log(getNameProp(car)); 
