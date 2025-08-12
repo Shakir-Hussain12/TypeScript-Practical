278103fa7125e844c1cef2e7efa486c8ea796c16
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-12 11:16:35 +0500

    added a getter

--- van-typescript/src/classes.ts
+++ van-typescript/src/classes.ts
@@ -1,12 +1,20 @@
 class Book {
-    title: string;
-    authors: string[];
+    
+    private title: string;
+    private authors: string[];
 
     constructor(title: string, authors: string[]) {
         this.title = title;
         this.authors = authors;
     }
+
+    getDetails(): { title: string; authors: string[] } {
+        return {
+            title: this.title,
+            authors: this.authors
+        };
+    }
 }
 
 const book = new Book("The Great Gatsby", ["F. Scott Fitzgerald"]);
-console.log(book);
+console.log(book.getDetails());
