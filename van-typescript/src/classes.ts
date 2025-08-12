a0deb843a68beed59d0c08e34f2dc1ef336ea192
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-12 11:48:41 +0500

    Implemented class with interface

--- van-typescript/src/classes.ts
+++ van-typescript/src/classes.ts
@@ -1,20 +1,51 @@
 class Book {
-    
-    private title: string;
-    private authors: string[];
+     
+    private readonly test = false;
+    constructor (
+        private title: string, 
+        private authors: string[]
+    ) {}
 
-    constructor(title: string, authors: string[]) {
-        this.title = title;
-        this.authors = authors;
-    }
-
-    getDetails(): { title: string; authors: string[] } {
+    get getDetails(): { title: string, authors: string[] } {
         return {
             title: this.title,
             authors: this.authors
         };
     }
+
+    isAlterable(): boolean {
+        // this.test = true; // This line is intentionally incorrect to demonstrate the error
+        return this.test;
+    }
 }
 
 const book = new Book("The Great Gatsby", ["F. Scott Fitzgerald"]);
-console.log(book.getDetails());
+console.log(book.getDetails);
+console.log("Is book alterable?", book.isAlterable());
+
+interface GenPerson {
+    name: string;
+    age: number;
+    getDetails : { name: string; age: number };
+    greet(): string;
+}
+
+class Person implements GenPerson {
+
+    constructor(public readonly name: string, public readonly age: number) {}
+
+    greet(): string {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+
+    get getDetails(): { name: string; age: number} {
+        return {
+            name: this.name,
+            age: this.age
+        };
+    }
+}
+
+const person = new Person("Alice", 30);
+console.log(person.greet());
+console.log(person.getDetails);
