f7100cd3bfc6fb9636dd6b40cbc3319b24c8788a
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-13 12:53:47 +0500

    General Layout for adding a task

--- van-typescript/src/tasks.ts
+++ van-typescript/src/tasks.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+
+const addTask = (): void => {
+    console.log("inside function");
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inp = document.querySelector<HTMLInputElement>('.form-input')!;
+    console.log(inp.value);
+    addTask();
+});
