ead0742b345a6eee15ffb70521b81984ce2d5417
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-14 12:38:18 +0500

    tasks project completed with plain TS

--- van-typescript/src/tasks.ts
+++ van-typescript/src/tasks.ts
@@ -3,29 +3,50 @@ type Task = {
     isCompleted: boolean
 
 }
+const list = document.querySelector<HTMLUListElement>('.list')!; 
 const tasks: Task[] = JSON.parse(localStorage.getItem('tasks') || '[]');
 
-const list = document.querySelector<HTMLUListElement>('.list')!;
+const updateLocalStorage = (): void => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const renderTask = (task: Task): void => {
+    //Adding checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.isCompleted;
+
+    //Event listener
+    checkbox.addEventListener('change', () => {
+        task.isCompleted = !task.isCompleted;
+        updateLocalStorage();
+    });
+
+    //Append to parent Node
+    const taskElement = document.createElement('li');
+    taskElement.textContent = task.description;
+    list.appendChild(taskElement);
+    taskElement.appendChild(checkbox);
+};
+
 const renderTasks = (): void => {
     list.innerHTML = '';
-    if (tasks.length > 0) {
-        tasks.forEach((task) => {
-            console.log('here')
-            list.appendChild(document.createElement('li')).innerText = task.description;
-        });
-    }
+    tasks?.forEach((task) => {
+        renderTask(task);            
+    });
 }
 renderTasks();
+
 
 const addTask = (task: Task): void => {
     //Add task to existing array
     tasks.push(task);
 
-    //Add to local Storage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    //Update local Storage
+    updateLocalStorage();
 
     //render
-    renderTasks();
+    renderTask(task);
 }
 
 const form = document.querySelector<HTMLFormElement>('.form')!;
