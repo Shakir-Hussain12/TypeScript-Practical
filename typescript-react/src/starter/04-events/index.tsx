45b31ab46be1ca58f2cb93fe8adbada792ec057c
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-28 10:30:26 +0500

    basic controlled form layout

--- typescript-react/src/starter/04-events/index.tsx
+++ typescript-react/src/starter/04-events/index.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 function Component() {
+
+  const [text, setText] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
+    <section>
+      <h2>Controlled Form</h2>
+      <form className="form">
+        <input 
+          type="text"
+          className="form-input mb-1" 
+          value={text} 
+          placeholder="Enter Text" 
+          onChange={(e) => setText(e.target.value)}
+          />
+        <input type="email" 
+          className="form-input mb-1" 
+          value={email} 
+          placeholder="Please Enter Email" 
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </form>
+    </section>
   );
 }
 export default Component;
