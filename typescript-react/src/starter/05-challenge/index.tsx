5291ca07e0235be04ed78aefbd9ceb09367fa66a
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-08-28 11:44:19 +0500

    refactoring

--- typescript-react/src/starter/05-challenge/index.tsx
+++ typescript-react/src/starter/05-challenge/index.tsx
@@ -16,20 +16,20 @@ type propType = PropsWithChildren<prop1 | prop2>
 function Component(props: propType) {
   if (props.type === 'basic') {
     return (
-      <div className="alert-success">
+      <div className="alert alert-success">
         <h2>Name: {props.name}</h2>
-        {props.children}
-      </div>
-    )
-  } else {
-    return (
-      <div className="alert-danger">
-        <h2>Name: {props.name}</h2>
-        <h2>Email: {props.email}</h2>
         {props.children}
       </div>
     )
   }
+
+  return (
+    <div className="alert alert-danger">
+      <h2>Name: {props.name}</h2>
+      <h2>Email: {props.email}</h2>
+      {props.children}
+    </div>
+  )
 }
 
 export default Component;
