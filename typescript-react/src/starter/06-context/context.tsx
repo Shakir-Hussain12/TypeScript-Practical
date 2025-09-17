9140ea134134339b657e5315caf5a0b532428c34
Shakir Hussain <shakir.hussain6969@gmail.com>
2025-09-17 17:10:58 +0500

    Theme Function

--- typescript-react/src/starter/06-context/context.tsx
+++ typescript-react/src/starter/06-context/context.tsx
@@ -3,7 +3,19 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext<string | undefined>(undefined);
 
 export function ThemeProvider({ children }: {children: React.ReactNode}) {
-    return <ThemeContext.Provider value="Hello">
-        {children}
-    </ThemeContext.Provider>
+    return (
+        <ThemeContext.Provider value="Hello">
+            {children} 
+        </ThemeContext.Provider>
+    )
 } 
+
+export const useTheme = () => {
+    const currTheme = useContext(ThemeContext);
+
+    if (currTheme === undefined) {
+        throw new Error("No Theme Defined");
+    }
+
+    console.log(currTheme);
+}
